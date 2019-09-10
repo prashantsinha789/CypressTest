c389d134b7874b56f48d3e6adee182b38183523b
Prashant Sinha <prashant.sinha@hobsons.com>
2019-09-10 03:11:04 -0400

    LDAP Broken Test Fixed

--- cypress/integration/ldapIntegration.spec.js
+++ cypress/integration/ldapIntegration.spec.js
@@ -1,64 +1,72 @@
-import {login} from "../pageObjects/loginPage"
-import {ldapConf} from "../pageObjects/ldapAddConfigPage"
-import {ldapTestTool} from "../pageObjects/ldapTesterPage"
-import {removeLdap} from "../pageObjects/removeLdapConfig"
-const mockData =  require("../fixtures/ldapMock");
+import {
+  login
+} from "../pageObjects/loginPage"
+import {
+  ldapConf
+} from "../pageObjects/ldapAddConfigPage"
+import {
+  ldapTestTool
+} from "../pageObjects/ldapTesterPage"
+import {
+  removeLdap
+} from "../pageObjects/removeLdapConfig"
+const mockData = require("../fixtures/ldapMock");
 
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 })
 
-describe('Ldap ConfigFiles Entries in SFAdmin',() =>  {
+describe('Ldap ConfigFiles Entries in SFAdmin', () => {
 
-    it('Ldap should be acquired on SF',() =>  {
-        login.loginInStageRC();
-        login.loginClickButtonOnSFAdmin();
+  it('Ldap should be acquired on SF', () => {
 
-        ldapConf.goToTenantConfig();             //Go to tenant and select the configs
-        ldapConf.setLdapUrl();                   //Add Config Value for LDAP_URL
-        ldapConf.setLdapServiceUsername();       //Add Config Value for LDAP_SERVICE_USERNAME
-        ldapConf.setLdapPassword();              //Add Config value for LDAP Password
-        ldapConf.setLdapBaseDN();                //Add Config Value for LdapBaseDN
-        ldapConf.setLdapFilter();                //Add Config Value for LdapFilter
-        ldapConf.editLocalLoginStategy();        //Edit localLoginStrategy
- })
+    login.loginInStageRC();
+    login.loginClickButtonOnSFAdmin();
 
-it('Verify the Ldap Setup through Ldap Tester Tools',() => {
+    ldapConf.goToTenantConfig(); //Go to tenant and select the configs
+    ldapConf.setLdapUrl(); //Add Config Value for LDAP_URL
+    ldapConf.setLdapServiceUsername(); //Add Config Value for LDAP_SERVICE_USERNAME
+    ldapConf.setLdapPassword(); //Add Config value for LDAP Password
+    ldapConf.setLdapBaseDN(); //Add Config Value for LdapBaseDN
+    ldapConf.setLdapFilter(); //Add Config Value for LdapFilter
+    ldapConf.editLocalLoginStategy(); //Edit localLoginStrategy
+  })
+
+  it('Verify the Ldap Setup through Ldap Tester Tools', () => {
 
     login.loginInLdapTesterStageRCAdmin();
     login.loginClickButtonOnSFAdmin();
     ldapTestTool.testerTools();
     cy.wait(20000);
-})
+  })
 
-it('Verify the Ldap SSO on Starfish-StageRC',() => {
-      login.loginInSFStageRC();
-      login.loginClickButtonOnSFUI();
-      login.loginInLdapStageRC();
-      login.loginClickButtonOnSFUI();
-      cy.wait(5000);
-      cy.url().should('include', mockData.SFDashboard);
-})
+  it('Verify the Ldap SSO on Starfish-StageRC', () => {
 
-it('Remove all Ldap configs from SFAdmin',() => {
+    login.loginInSFStageRC();
+    login.loginClickButtonOnSFUI();
+    login.loginInLdapStageRC();
+    login.loginClickButtonOnSFUI();
+    cy.wait(5000);
+    cy.url().should('include', mockData.SFDashboard);
+  })
 
-  login.loginInStageRC();
-  login.loginClickButtonOnSFAdmin();
-  removeLdap.verifyLdapConfig();
+  it('Remove all Ldap configs from SFAdmin', () => {
 
-  /* Removing Ldap Configs from SFAdmin StageRC */
+    login.loginInStageRC();
+    login.loginClickButtonOnSFAdmin();
+    removeLdap.verifyLdapConfig();
+    /* Removing Ldap Configs from SFAdmin StageRC */
+    removeLdap.removeLdapBaseDN();
+    removeLdap.removeLdapURL();
+    removeLdap.removeLdapUserName();
+    removeLdap.removeLdapFilter();
+    removeLdap.removeLdapPassword();
+    removeLdap.editLocalLogin();
+  })
 
-  removeLdap.removeLdapBaseDN();
-  removeLdap.removeLdapURL();
-  removeLdap.removeLdapUserName();
-  removeLdap.removeLdapFilter();
-  removeLdap.removeLdapPassword();
-  removeLdap.editLocalLogin();
-})
+  it('Verify the Login on Starfish-StageRC', () => {
 
-it('Verify the Login on Starfish-StageRC',() => {
-  login.loginInSFStageRC();
-  login.loginClickButtonOnSFUI();
-
+    login.loginInSFStageRC();
+    login.loginClickButtonOnSFUI();
   })
 })
